3f6cdc2fc4e9b8be2b8aebecf4f7168c5eee5ba0
Guna-ME <Dudamarcos151@hotmail.com>
2020-08-31 14:56:47 -0300

    feat: adiciona exercício 07

---
+++ exercicio07.py
@@ -0,0 +1,6 @@
+fahrenheit  = 0
+celsius     = 0
+
+fahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
+celsius     = 5.0 * (fahrenheit - 32.0) / 9.0
+print("A temperatura em celsius é:", celsius)
