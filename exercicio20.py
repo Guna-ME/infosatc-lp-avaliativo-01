4091ba79a974a021fc88eeccd2f10db036923a6f
Guna-ME <Dudamarcos151@hotmail.com>
2020-09-01 10:03:24 -0300

    fix: altera variável

--- exercicio20.py
+++ exercicio20.py
@@ -1,6 +1,6 @@
-quilogramas = 0
+quilograma = 0
 litros      = 0
 
-quilogramas = float(input("Digite a massa em quilogramas: "))
-litros      = quilogramas * 0.45
-print("O valor da massa em litros é: ", litros)
+quilograma = float(input("Digite a massa em quilogramas: "))
+litros      = quilograma * 0.45
+print("O valor da massa em litros é: ", litros) 
