d50bfb382947fe8b29c0fca90abda247d78ac991
Guna-ME <Dudamarcos151@hotmail.com>
2020-09-01 10:17:50 -0300

    feat: adiciona exercício 29

---
+++ exercicio29.py
@@ -0,0 +1,13 @@
+nota1   = 0
+nota2   = 0
+nota3   = 0
+nota4   = 0
+mediaA  = 0
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
+
+mediaA = nota1 + nota2 + nota3 + nota4 / 4
+print("A média aritimetica das notas foi: ", mediaA)S
