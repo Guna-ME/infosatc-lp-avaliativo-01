17ea351862f2f02f71eae7e567a9551f9d52d205
Guna-ME <Dudamarcos151@hotmail.com>
2020-09-01 09:47:00 -0300

    feat: adiciona exercício 24

---
+++ exercicio24.py
@@ -0,0 +1,6 @@
+metrosQuadrados = 0
+acres           = 0
+
+metrosQuadrados = float(input("Digite um valor em metros quadrados: "))
+acres           = metrosQuadrados * 0.000247
+print("O valor em acres é: ", acres)
