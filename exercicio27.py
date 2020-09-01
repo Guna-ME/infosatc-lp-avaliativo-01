975c1bcfd894bfd51879dc6d1390413fbbcdcf25
Guna-ME <Dudamarcos151@hotmail.com>
2020-09-01 10:09:50 -0300

    feat: adiciona exercício 27

---
+++ exercicio27.py
@@ -0,0 +1,6 @@
+metrosQuadrados = 0
+hectares        = 0
+
+hectares        = float(input("Digite o valor da área em hectares: "))
+metrosQuadrados = hectares * 10000
+print("O valor da área em metros quadrados é: ", metrosQuadrados)
