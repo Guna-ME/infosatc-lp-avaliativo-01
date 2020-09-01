d6fe846f64b7ca5dbabb9b30a9da49376fa94b24
Guna-ME <Dudamarcos151@hotmail.com>
2020-09-01 09:33:21 -0300

    fix: altera variável

--- exercicio07.py
+++ exercicio07.py
@@ -2,5 +2,5 @@ fahrenheit  = 0
 celsius     = 0
 
 fahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
-celsius     = 5.0 * (fahrenheit - 32.0) / 9.0
+celsius    = 5.0 * (fahrenheit - 32.0) / 9.0
 print("A temperatura em celsius é:", celsius)
